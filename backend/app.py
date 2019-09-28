575985c45d6217f68fbdc688a969057535a42f77
Maxim Adrian <maksim.adrian76@gmail.com>
2019-09-28 22:47:05 +0300

    Вынес инициализацию приложения из проверки на __main__

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 app.register_blueprint(bp_api, url_prefix="/api")
 
+
+app.config.from_pyfile("config.cfg")
+print(app.url_map)
+print(app.config)
+
 if __name__ == '__main__':
-    app.config.from_pyfile("config.cfg")
-    print(app.url_map)
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8000)
