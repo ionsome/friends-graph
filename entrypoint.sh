0e7499178e90353aa34ce751513b5c948d7812a5
Maxim Adrian <maksim.adrian76@gmail.com>
2019-10-30 21:21:24 +0300

    Добавил амперсанд в скрипт

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set +e
 
 cd /backend || exit
 
-gunicorn -b 0.0.0.0:$PORT --worker-class gevent --worker-connections 1024 app:app
+gunicorn -b 0.0.0.0:$PORT --worker-class gevent --worker-connections 1024 app:app &
