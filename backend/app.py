b1ad060113c5f446f36db2192494e5e49e55cfe7
Maxim Adrian <maksim.adrian76@gmail.com>
2019-10-30 16:25:35 +0300

    Bugfix

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+import os
 
 from api.core import bp_api
 
 app = Flask(__name__)
 
-app.register_blueprint(bp_api, url_prefix='/api')
+app.register_blueprint(bp_api, url_prefix="/api")
 
 
-app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
-app.config.from_pyfile('config.cfg')
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config.from_pyfile("config.cfg")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
