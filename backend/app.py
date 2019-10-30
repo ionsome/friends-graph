ec38bc55083d0f0505e682cfbd5d306202932087
Maxim Adrian <maksim.adrian76@gmail.com>
2019-10-30 22:17:34 +0300

    Добавил второй тестовый контейнер

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+import requests
 import os
 
 from api.core import bp_api
 
 app = Flask(__name__)
-
-app.register_blueprint(bp_api, url_prefix="/api")
-
-
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config.from_pyfile("config.cfg")
+
+
+@app.route('/')
+def index():
+    return requests.get('http://localhost:31337/').text
+
+app.register_blueprint(bp_api, url_prefix="/api")
 
 
 if __name__ == "__main__":
