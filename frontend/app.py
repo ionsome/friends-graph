c99981e76504534c258f4e0b5f030e4956631112
Maxim Adrian <maksim.adrian76@gmail.com>
2019-11-10 03:09:22 +0300

    Добавил шаблон с авторизацией

--- frontend/app.py
+++ frontend/app.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_url_path='/static')
+from flask_cors import CORS
 
+app = Flask(__name__, static_url_path='')
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, resources={'/auth': {"origins": "http://http://2.94.11.10:5000/"}})
 
 @app.route('/')
 def index():
     """Возвращает текущий шаблон из static."""
-    return app.send_static_file('index.html')
+    return send_from_directory('static', 'index.html')
+
+
+@app.route('/auth')
+def auth():
+    """Возвращает текущий шаблон из static."""
+    return send_from_directory('static', 'auth.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0')
