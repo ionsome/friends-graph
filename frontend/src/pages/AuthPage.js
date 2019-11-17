2eef008a5e5b4aa73cb7bc4cdcbf3b3e3906cb9e
Maxim Adrian <maksim.adrian76@gmail.com>
2019-11-17 19:32:10 +0300

    Переписал логику кнопок на страница авторизации

--- frontend/src/pages/AuthPage.js
+++ frontend/src/pages/AuthPage.js
@@ -6,27 +6,13 @@ const VK = window.VK;
 
 class AuthPage extends Component {
 
-     loginButtonClickHandler = () => {
-        let popup = window.open("https://oauth.vk.com/authorize?client_id=7154329&display=popup&redirect_uri=close.html&scope=friends&response_type=code&v=5.101", 'test', "height=200,width=200");
-        let timer = setInterval(function() {
-            if(popup.closed) {
-                clearInterval(timer);
-                window.location.href = '/';
-            }
-        }, 1000);
+    loginButtonClickHandler = () => {
+        VK.Auth.login(() => window.location.reload(true));
     };
 
     statusButtonClickHandler = () => {
-        VK.Auth.getLoginStatus(function(response) {
-            alert(response);
-            if (response.session) {
-                // Пользователь успешно авторизовался
-                if (response.settings) {
-                    // Выбранные настройки доступа пользователя, если они были запрошены
-                }
-            } else {
-                // Пользователь нажал кнопку 'Отмена' в окне авторизации
-            }
+        VK.Auth.getLoginStatus(function (response) {
+            console.log(response);
         });
     };
 
