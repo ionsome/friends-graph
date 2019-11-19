4111157150f221865a5852d82b5226785b0051ee
Nikita Kuchur <nikitakuchur@gmail.com>
2019-11-19 23:48:56 +0300

    Обновил сайдбар

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar, Button } from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
 import logo from '../res/logo.svg';
-
-const VK = window.VK;
+import hide from '../res/hide.svg'
 
 class Header extends Component {
-
-    logoutButtonClickHandler = () => {
-        VK.Auth.logout();
-        window.location.href = '/';
-    };
 
     render() {
         return (
@@ -18,7 +12,7 @@ class Header extends Component {
                     <img alt="" src={logo} width="30" height="30" className="d-inline-block align-top mr-2" />
                     Friends Graph
                 </Navbar.Brand>
-                <Button variant="primary" className="ml-auto" onClick={this.logoutButtonClickHandler}>Log out</Button>
+                <img alt="" src={hide} width="30" height="30" className="d-inline-block align-top ml-auto" />
             </Navbar>
         );
     }
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
-import { Card, FormControl } from "react-bootstrap";
+import {Button, Card, FormControl} from "react-bootstrap";
 import ListView from "./ListView";
 import Header from "./Header";
 
+const VK = window.VK;
+
 class Sidebar extends Component {
+
+    logoutButtonClickHandler = () => {
+        VK.Auth.logout();
+        window.location.href = '/';
+    };
 
     render() {
         return (
             <div className="d-flex flex-fill">
-                <Card bg="light" className="border-left-0 border-top-0 border-bottom-0">
+                <Card bg="light" className="border-left-0 border-top-0 border-bottom-0" style={{width: "300px"}}>
                     <Header/>
                     <FormControl className="m-2 w-auto" type="search" placeholder="Search" aria-label="Search"/>
                     <ListView/>
+                    <Card.Footer className="d-flex">
+                        <Button className="ml-auto" onClick={this.logoutButtonClickHandler}>Log out</Button>
+                    </Card.Footer>
                 </Card>
             </div>
         );
