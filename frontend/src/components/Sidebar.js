21e75d1ca0a17081bf5f828eed3e35067525a474
Maxim Adrian <maksim.adrian76@gmail.com>
2019-11-20 01:21:33 +0300

    Перенёс reload после logout в callback и сделал принудительной с сервера

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Button, Card, FormControl} from "react-bootstrap";
+import { Button, Card, FormControl } from "react-bootstrap";
 import ListView from "./ListView";
 import Header from "./Header";
 
@@ -8,17 +8,17 @@ const VK = window.VK;
 class Sidebar extends Component {
 
     logoutButtonClickHandler = () => {
-        VK.Auth.logout();
-        window.location.href = '/';
+        VK.Auth.logout(() =>
+            window.location.reload(true));
     };
 
     render() {
         return (
             <div className="d-flex flex-fill">
-                <Card bg="light" className="border-left-0 border-top-0 border-bottom-0" style={{width: "300px"}}>
-                    <Header/>
-                    <FormControl className="m-2 w-auto" type="search" placeholder="Search" aria-label="Search"/>
-                    <ListView/>
+                <Card bg="light" className="border-left-0 border-top-0 border-bottom-0" style={{ width: "300px" }}>
+                    <Header />
+                    <FormControl className="m-2 w-auto" type="search" placeholder="Search" aria-label="Search" />
+                    <ListView />
                     <Card.Footer className="d-flex">
                         <Button className="ml-auto" onClick={this.logoutButtonClickHandler}>Log out</Button>
                     </Card.Footer>
