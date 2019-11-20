5fbe342e061accbae58c9603ab6fe522b82d0838
Nikita Kuchur <nikitakuchur@gmail.com>
2019-11-20 14:07:37 +0300

    Немного поправил сайдбар

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, FormControl } from "react-bootstrap";
+import {Button, Card, FormControl, Navbar} from "react-bootstrap";
 import ListView from "./ListView";
 import Header from "./Header";
 
@@ -15,11 +15,11 @@ class Sidebar extends Component {
     render() {
         return (
             <div className="d-flex flex-fill">
-                <Card bg="light" className="border-left-0 border-top-0 border-bottom-0" style={{ width: "300px" }}>
-                    <Header />
+                <Card bg="light" className="border-left-0 border-top-0 border-bottom-0" style={{ width: "280px" }}>
+                    <Header/>
                     <FormControl className="m-2 w-auto" type="search" placeholder="Search" aria-label="Search" />
                     <ListView />
-                    <Card.Footer className="d-flex">
+                    <Card.Footer className="d-flex p-2">
                         <Button className="ml-auto" onClick={this.logoutButtonClickHandler}>Log out</Button>
                     </Card.Footer>
                 </Card>
