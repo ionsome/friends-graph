eb78ad72c1d53dd3cf915a24d4b2f54db36bfde0
Maxim Adrian <maksim.adrian76@gmail.com>
2019-11-28 22:52:28 +0300

    Сделал добавление ребёр

--- frontend/src/models/Users.js
+++ frontend/src/models/Users.js
@@ -5,26 +5,58 @@ import { users_get, friends_get } from '../api/Friends';
 let users = [];
 let relations = [];
 
-let createProfile = (container) => new Profile(container.id, container.first_name, container.last_name);
-
+/*
+    Добавляет пользователя и всех его друзей.
+*/
 async function addRootUser(id, graph) {
-    let root_user = await users_get(id);
-
-    let profile = createProfile(root_user[0]);
-    users.push(profile);
-    graph.addNodes([profile]);
+    let rootUser = createProfileById(id);
+    addUser(rootUser, graph);
 
     let friends = await friends_get(id);
     for (const friend of friends) {
-        await addUser(friend, graph);
+        let profile = createProfileByData(friend);
+        addUser(profile);
+    };
+
+    await addUserRelations(rootUser, graph);
+    for (const friend of friends) {
+        await addUserRelations(friend, graph);
     };
 }
 
-async function addUser(id, graph) {
-    let result = await users_get(id);
-    if (!users.some((element) => result === undefined || element.id === result.id))
-        graph.addNodes([createProfile(result[0])])
+async function createProfileById(id) {
+    let data = await users_get(id);
+    if (!users.some((element) => data === undefined || element.id === data.id)) {
+        return createProfileByData(data);
+    }
+};
+
+function createProfileByData(data) {
+    return new Profile(data.id, data.first_name, data.last_name);
+}
+
+
+function addUser(profile, graph) {
+    users.push(profile);
+    graph.addNodes([profile]);
+};
+
+/*
+    Добавляет связи между текущим пользователем и теми, кто
+    уже добавлен в users.
+*/
+async function addUserRelations(profile, graph, friends) {
+    friends = friends || [];
+    if (friends) {
+        friends = await friends_get(profile.id, true);
+    }
+    let user_ids = users.map(elem => elem.id);
+    for (const friend_id of friends) {
+        if (friend_id in user_ids) {
+            graph.addEdges([{from: profile.id, to: friend_id}]);
+        }
+    }
 };
 
 
-export { users, relations, addUser, addRootUser };
+export { users, relations, createProfileById, addUser, addRootUser };
