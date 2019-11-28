8e27d3e044d37041d2da9756b8812d206ee19e2c
Nikita Kuchur <nikitakuchur@gmail.com>
2019-11-28 11:15:25 +0300

    Добавил отображение аватарок

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import { ListGroup } from "react-bootstrap";
+import { Button, ListGroup } from "react-bootstrap";
+import defaultAvatar from "../res/default-avatar.png";
 
 class ListView extends Component {
 
     render() {
         return (
             <ListGroup id="list-view" className="text-nowrap">
-                <ListGroup.Item action onClick={() => {this.props.onItemClick("Content #1")}}>
-                    Item #1
+                <ListGroup.Item>
+                    <Button onClick={() => {this.props.onItemClick("Content #1")}} variant="sidebar-light" className="text-left w-100">
+                        <img alt="photo" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
+                        Item #1
+                    </Button>
                 </ListGroup.Item>
-                <ListGroup.Item action onClick={() => {this.props.onItemClick("Content #2")}}>
-                    Item #2
+                <ListGroup.Item>
+                    <Button onClick={() => {this.props.onItemClick("Content #2")}} variant="sidebar-light" className="text-left w-100">
+                        <img alt="photo" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
+                        Item #2
+                    </Button>
                 </ListGroup.Item>
-                <ListGroup.Item action onClick={() => {this.props.onItemClick("Content #3")}}>
-                    Item #3
+                <ListGroup.Item>
+                    <Button onClick={() => {this.props.onItemClick("Content #3")}} variant="sidebar-light" className="text-left w-100">
+                        <img alt="photo" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
+                        Item #3
+                    </Button>
                 </ListGroup.Item>
-                <ListGroup.Item action onClick={() => {this.props.onItemClick("Content #4")}}>
-                    Item #4
+                <ListGroup.Item>
+                    <Button onClick={() => {this.props.onItemClick("Content #4")}} variant="sidebar-light" className="text-left w-100">
+                        <img alt="photo" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
+                        Item #4
+                    </Button>
                 </ListGroup.Item>
-                <ListGroup.Item action onClick={() => {this.props.onItemClick("Content #5")}}>
-                    Item #5
+                <ListGroup.Item>
+                    <Button onClick={() => {this.props.onItemClick("Content #5")}} variant="sidebar-light" className="text-left w-100">
+                        <img alt="photo" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
+                        Item #5
+                    </Button>
                 </ListGroup.Item>
             </ListGroup>
         );
