ed517dcba39e72d30cfb0592099b8a54017b4b3d
Maxim Adrian <maksim.adrian76@gmail.com>
2019-11-29 14:13:32 +0300

    Сменил дефолтного юзера

--- frontend/src/components/FriendsGraph.js
+++ frontend/src/components/FriendsGraph.js
@@ -80,7 +80,7 @@ class FriendsGraph extends Component {
     test_api() {
         this.addNodes({ label: "Maxim", image:'https://sun1-28.userapi.com/c850424/v850424298/65692/uCP5eFwQCBI.jpg?ava=2'});
         this.addEdges([{ from: 4, to: 6 }]);
-        addRootUser(97992816, this);
+        addRootUser(213966324, this);
     }
 
     addNodes(nodes) {
