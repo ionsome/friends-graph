178f5319b1596d373e868f79c50cd5feb4fe47db
Nikita Kuchur <nikitakuchur@gmail.com>
2019-11-29 11:50:36 +0300

    Отредактировал манифест, добавил иконку и название

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -9,31 +9,31 @@ class ListView extends Component {
             <ListGroup id="list-view" className="text-nowrap">
                 <ListGroup.Item>
                     <Button onClick={() => {this.props.onItemClick("Content #1")}} variant="sidebar-light" className="text-left w-100">
-                        <img alt="photo" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
+                        <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
                         Item #1
                     </Button>
                 </ListGroup.Item>
                 <ListGroup.Item>
                     <Button onClick={() => {this.props.onItemClick("Content #2")}} variant="sidebar-light" className="text-left w-100">
-                        <img alt="photo" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
+                        <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
                         Item #2
                     </Button>
                 </ListGroup.Item>
                 <ListGroup.Item>
                     <Button onClick={() => {this.props.onItemClick("Content #3")}} variant="sidebar-light" className="text-left w-100">
-                        <img alt="photo" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
+                        <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
                         Item #3
                     </Button>
                 </ListGroup.Item>
                 <ListGroup.Item>
                     <Button onClick={() => {this.props.onItemClick("Content #4")}} variant="sidebar-light" className="text-left w-100">
-                        <img alt="photo" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
+                        <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
                         Item #4
                     </Button>
                 </ListGroup.Item>
                 <ListGroup.Item>
                     <Button onClick={() => {this.props.onItemClick("Content #5")}} variant="sidebar-light" className="text-left w-100">
-                        <img alt="photo" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
+                        <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
                         Item #5
                     </Button>
                 </ListGroup.Item>
