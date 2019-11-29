1764fae7bb6372cae3238c5aa730f8b95163b0ef
Maxim Adrian <maksim.adrian76@gmail.com>
2019-11-30 01:47:49 +0300

    Добавил попытку отобразить юзеров в listview

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -1,42 +1,36 @@
 import React, { Component } from 'react';
 import { Button, ListGroup } from "react-bootstrap";
+import { users } from "../models/Users";
 import defaultAvatar from "../res/default-avatar.png";
 
 class ListView extends Component {
 
+
     render() {
         return (
             <ListGroup id="list-view" className="text-nowrap">
-                <ListGroup.Item>
-                    <Button onClick={() => {this.props.onItemClick("Content #1")}} variant="sidebar-light" className="text-left w-100">
-                        <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
-                        Item #1
-                    </Button>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <Button onClick={() => {this.props.onItemClick("Content #2")}} variant="sidebar-light" className="text-left w-100">
-                        <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
-                        Item #2
-                    </Button>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <Button onClick={() => {this.props.onItemClick("Content #3")}} variant="sidebar-light" className="text-left w-100">
-                        <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
-                        Item #3
-                    </Button>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <Button onClick={() => {this.props.onItemClick("Content #4")}} variant="sidebar-light" className="text-left w-100">
-                        <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
-                        Item #4
-                    </Button>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    <Button onClick={() => {this.props.onItemClick("Content #5")}} variant="sidebar-light" className="text-left w-100">
-                        <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4"/>
-                        Item #5
-                    </Button>
-                </ListGroup.Item>
+                {users.map((card) => {
+                    return (
+                        <ListGroup.Item>
+                            <Button onClick={() => { this.props.onItemClick("Content " + card.id); }} variant="sidebar-light" className="text-left w-100">
+                                <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4" />
+                                Item #{card.label}
+                            </Button>
+                        </ListGroup.Item>
+                    );
+                })
+                }
+                {[1,2,3,4,5].map((card) => {
+                    return (
+                        <ListGroup.Item>
+                            <Button onClick={() => { this.props.onItemClick("Content " + card); }} variant="sidebar-light" className="text-left w-100">
+                                <img alt="" src={defaultAvatar} width="30" height="30" className="avatar mr-4" />
+                                Item #{card}
+                            </Button>
+                        </ListGroup.Item>
+                    );
+                })
+                }
             </ListGroup>
         );
     }
