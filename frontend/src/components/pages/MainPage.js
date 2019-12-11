72436d7a6ce86bfab023517992bd9a568befe7ab
Maxim Adrian <maksim.adrian76@gmail.com>
2019-12-11 23:15:52 +0300

    Убрал передачу добавления обычного юзера, добавил передачу создания профиля

--- frontend/src/components/pages/MainPage.js
+++ frontend/src/components/pages/MainPage.js
@@ -8,23 +8,33 @@ class MainPage extends Component {
   render() {
     return (
       <Grapharable>
-        {(users, relations, addRootUser, addUser, removeUser, bindGraph) => (
-          <div className="main d-flex">
-            <Graph
-              users={users}
-              relations={relations}
-              addRootUser={(id, network) => addRootUser(id, network)}
-              bindGraph={bindGraph}
-              addUser={addUser}
-            />
-            <Sidebar
-              userList={users}
-              changeIsAuthorized={this.props.changeIsAuthorized}
-              addRootUser={addRootUser}
-              removeUser={removeUser}
-            />
-          </div>
-        )}
+        {(users, relations, createProfileById, addRootUser, removeUser, bindGraph) => {
+          let defaultUser = {
+            "id": 30042239,
+            "label": "Алексей Русалкин",
+            "color": "",
+            "image": "https://sun1-17.userapi.com/8R582ps0d9RxUwDsiMy8GUrg3hNKfXTbOWIqpQ/bODgNmoNGys.jpg?ava=1",
+            "root": false
+          };
+          //createProfileById(213966324);
+          return (
+            <div className="main d-flex">
+              <Graph
+                users={users}
+                relations={relations}
+                addRootUser={addRootUser}
+                bindGraph={bindGraph}
+              />
+              <Sidebar
+                userList={users}
+                changeIsAuthorized={this.props.changeIsAuthorized}
+                addRootUser={addRootUser}
+                removeUser={removeUser}
+                defaultUser={defaultUser}
+              />
+            </div>
+          );
+        }}
       </Grapharable>
     );
   }
