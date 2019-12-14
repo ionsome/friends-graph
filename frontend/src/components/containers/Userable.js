769f11551482360eaa9aba83b93cf99b9271d953
Maxim Adrian <maksim.adrian76@gmail.com>
2019-12-14 13:45:35 +0300

    Продолжение фикса удаления

--- frontend/src/components/containers/Userable.js
+++ frontend/src/components/containers/Userable.js
@@ -156,10 +156,10 @@ class Userable extends Component {
   }
 
   removeUser(profile) {
-    //const index = this.state.users.indexOf(profile);
-    if (profile) {
+    if (profile && profile.id) {
+      this.users = this.users.filter(s => s.id !== profile.id)
       this.setState({
-        users: this.state.users.filter(s => s.id !== profile.id)
+        users: this.users
       });
       return true;
     }
